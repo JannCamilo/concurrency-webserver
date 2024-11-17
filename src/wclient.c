3f412cbe6dcfd634331dfc8d9988b209c8529d76
JannCamilo <janncamilo87@gmail.comjanncamilo87@gmail.com>
2024-11-17 21:09:07 +0100

    V1 concurrency

--- src/wclient.c
+++ src/wclient.c
@@ -1,92 +1,99 @@
-//
-// client.c: A very, very primitive HTTP client.
-// 
-// To run, try: 
-//      client hostname portnumber filename
-//
-// Sends one HTTP request to the specified HTTP server.
-// Prints out the HTTP response.
-//
-// For testing your server, you will want to modify this client.  
-// For example:
-// You may want to make this multi-threaded so that you can 
-// send many requests simultaneously to the server.
-//
-// You may also want to be able to request different URIs; 
-// you may want to get more URIs from the command line 
-// or read the list from a file. 
-//
-// When we test your server, we will be using modifications to this client.
-//
-
 #include "io_helper.h"
+#include <pthread.h>
 
 #define MAXBUF (8192)
 
-//
-// Send an HTTP request for the specified file 
-//
+// Estructura para pasar parámetros a los hilos
+typedef struct {
+    char *host;
+    int port;
+    char *filename;
+} thread_data_t;
+
+// Función para enviar la solicitud HTTP GET
 void client_send(int fd, char *filename) {
     char buf[MAXBUF];
     char hostname[MAXBUF];
     
     gethostname_or_die(hostname, MAXBUF);
     
-    /* Form and send the HTTP request */
+    // Formar y enviar la solicitud HTTP
     sprintf(buf, "GET %s HTTP/1.1\n", filename);
     sprintf(buf, "%shost: %s\n\r\n", buf, hostname);
     write_or_die(fd, buf, strlen(buf));
 }
 
-//
-// Read the HTTP response and print it out
-//
+// Función para leer y mostrar la respuesta HTTP
 void client_print(int fd) {
     char buf[MAXBUF];  
     int n;
     
-    // Read and display the HTTP Header 
+    // Leer y mostrar los encabezados HTTP
     n = readline_or_die(fd, buf, MAXBUF);
     while (strcmp(buf, "\r\n") && (n > 0)) {
-	printf("Header: %s", buf);
-	n = readline_or_die(fd, buf, MAXBUF);
-	
-	// If you want to look for certain HTTP tags... 
-	// int length = 0;
-	//if (sscanf(buf, "Content-Length: %d ", &length) == 1) {
-	//    printf("Length = %d\n", length);
-	//}
+        printf("Header: %s", buf);
+        n = readline_or_die(fd, buf, MAXBUF);
     }
     
-    // Read and display the HTTP Body 
+    // Leer y mostrar el cuerpo de la respuesta HTTP
     n = readline_or_die(fd, buf, MAXBUF);
     while (n > 0) {
-	printf("%s", buf);
-	n = readline_or_die(fd, buf, MAXBUF);
+        printf("%s", buf);
+        n = readline_or_die(fd, buf, MAXBUF);
     }
+}
+
+// Función que será ejecutada por cada hilo
+void *client_thread(void *arg) {
+    thread_data_t *data = (thread_data_t *)arg;
+    int clientfd;
+    
+    // Abrir la conexión con el servidor
+    clientfd = open_client_fd_or_die(data->host, data->port);
+    
+    // Enviar la solicitud y mostrar la respuesta
+    client_send(clientfd, data->filename);
+    client_print(clientfd);
+    
+    // Cerrar la conexión
+    close_or_die(clientfd);
+    
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
     char *host, *filename;
     int port;
-    int clientfd;
+    int num_threads = 10;  // Número de hilos para la concurrencia
+    pthread_t threads[num_threads];
+    thread_data_t data[num_threads];
     
     if (argc != 4) {
-	fprintf(stderr, "Usage: %s <host> <port> <filename>\n", argv[0]);
-	exit(1);
+        fprintf(stderr, "Usage: %s <host> <port> <filename>\n", argv[0]);
+        exit(1);
     }
     
     host = argv[1];
     port = atoi(argv[2]);
     filename = argv[3];
     
-    /* Open a single connection to the specified host and port */
-    clientfd = open_client_fd_or_die(host, port);
+    // Crear varios hilos para simular múltiples conexiones
+    for (int i = 0; i < num_threads; i++) {
+        data[i].host = host;
+        data[i].port = port;
+        data[i].filename = filename;
+        
+        // Crear el hilo
+        if (pthread_create(&threads[i], NULL, client_thread, (void *)&data[i]) != 0) {
+            fprintf(stderr, "Error to created the thread %d\n", i);
+            exit(1);
+        }
+    }
     
-    client_send(clientfd, filename);
-    client_print(clientfd);
-    
-    close_or_die(clientfd);
-    
+    // Esperar que todos los hilos terminen
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
     exit(0);
 }
--- src/wserver.c
+++ src/wserver.c
@@ -2,40 +2,153 @@
 #include "request.h"
 #include "io_helper.h"
 
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <errno.h>
+
+#define DEFAULT_PORT 8111
+#define DEFAULT_THREADS 1
+#define DEFAULT_BUFFERS 2
+#define BUFFER_SIZE 1024
 char default_root[] = ".";
 
+typedef struct {
+    int *buffer;
+    int size;
+    int start;
+    int end;
+    pthread_mutex_t lock;
+    pthread_cond_t not_empty;
+    pthread_cond_t not_full;
+} Buffer;
+
+Buffer *buffer_init(int size) {
+    Buffer *b = malloc(sizeof(Buffer));
+    b->buffer = malloc(sizeof(int) * size);
+    b->size = size;
+    b->start = 0;
+    b->end = 0;
+    pthread_mutex_init(&b->lock, NULL);
+    pthread_cond_init(&b->not_empty, NULL);
+    pthread_cond_init(&b->not_full, NULL);
+    return b;
+}
+
+void buffer_push(Buffer *b, int conn_fd) {
+    pthread_mutex_lock(&b->lock);
+    while ((b->end + 1) % b->size == b->start) {
+        printf("Buffer full, waiting space...\n");  // Mensaje cuando el buffer está lleno
+        pthread_cond_wait(&b->not_full, &b->lock); // Esperar si el buffer está lleno
+    }
+    b->buffer[b->end] = conn_fd;
+    b->end = (b->end + 1) % b->size;
+    printf("Conection %d added to the buffer (start: %d, end: %d)\n", conn_fd, b->start, b->end);  // Mensaje cuando se agrega una conexión
+    pthread_cond_signal(&b->not_empty); // Notificar que hay algo para procesar
+    pthread_mutex_unlock(&b->lock);
+}
+
+int buffer_pop(Buffer *b) {
+    pthread_mutex_lock(&b->lock);
+    while (b->start == b->end) {
+        printf("Buffer empty, waiting a new conecion...\n");  // Mensaje cuando el buffer está vacío
+        pthread_cond_wait(&b->not_empty, &b->lock); // Esperar si el buffer está vacío
+    }
+    int conn_fd = b->buffer[b->start];
+    b->start = (b->start + 1) % b->size;
+    printf("Conection %d sorted of the buffer (start: %d, end: %d)\n", conn_fd, b->start, b->end);  // Mensaje cuando se retira una conexión
+    pthread_cond_signal(&b->not_full); // Notificar que el buffer no está lleno
+    pthread_mutex_unlock(&b->lock);
+    return conn_fd;
+}
+
+void *worker(void *arg) {
+    Buffer *b = (Buffer *) arg;
+    printf("Thread of work %ld start.\n", pthread_self());  // Mensaje cuando un hilo de trabajo inicia
+
+    while (1) {
+        int conn_fd = buffer_pop(b); // Obtener una conexión del buffer
+        printf("Thread %ld proceing the conection %d.\n", pthread_self(), conn_fd);  // Mensaje cuando un hilo empieza a procesar una conexión
+        request_handle(conn_fd); // Procesar la solicitud del cliente
+        printf("Thread %ld finish of procesing the conection %d.\n", pthread_self(), conn_fd);  // Mensaje cuando un hilo termina de procesar
+        close_or_die(conn_fd); // Cerrar la conexión cuando termine
+    }
+}
+
 //
-// ./wserver [-d <basedir>] [-p <portnum>] 
+// ./wserver [-d basedir] [-p port] [-t threads] [-b buffers]
 // 
 int main(int argc, char *argv[]) {
     int c;
     char *root_dir = default_root;
-    int port = 10000;
+    int port = DEFAULT_PORT;
+    int thread_count = DEFAULT_THREADS;
+    int buffer_size = DEFAULT_BUFFERS;
     
-    while ((c = getopt(argc, argv, "d:p:")) != -1)
-	switch (c) {
-	case 'd':
-	    root_dir = optarg;
-	    break;
-	case 'p':
-	    port = atoi(optarg);
-	    break;
-	default:
-	    fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
-	    exit(1);
-	}
-
+    while ((c = getopt(argc, argv, "d:p:t:b")) != -1)
+        switch (c) {
+        case 'd':
+            root_dir = optarg;
+            break;
+        case 'p':
+            port = atoi(optarg);
+            if (port <= 0) {
+                fprintf(stderr, "Port invalid.\n");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        case 't':
+            thread_count = atoi(optarg);
+            if (thread_count <= 0) {
+                fprintf(stderr, "The number of threads should be a positive integer.\n");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        case 'b':
+            buffer_size = atoi(optarg);
+            if (buffer_size <= 0) {
+                fprintf(stderr, "The size of buffer shloud be a positive integer.\n");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        default:
+            fprintf(stderr, "usage: wserver [-d basedir] [-p port] [-t threads] [-b buffers]\n");
+            exit(1);
+        }
+    
     // run out of this directory
     chdir_or_die(root_dir);
 
-    // now, get to work
+    // Crear el buffer
+    Buffer *buffer = buffer_init(buffer_size);
+
+    // Crear hilos de trabajo
+    pthread_t threads[thread_count];
+    for (int i = 0; i < thread_count; ++i) {
+        if (pthread_create(&threads[i], NULL, worker, (void *)buffer) != 0) {
+            fprintf(stderr, "Error al crear hilo %d\n", i);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Abrir el socket de escucha
     int listen_fd = open_listen_fd_or_die(port);
     while (1) {
-	struct sockaddr_in client_addr;
-	int client_len = sizeof(client_addr);
-	int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
-	request_handle(conn_fd);
-	close_or_die(conn_fd);
+        printf("Started serverin the port %d whit %d threads and buffer of size %d\n", port, thread_count, buffer_size);
+        struct sockaddr_in client_addr;
+        int client_len = sizeof(client_addr);
+        int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr, (socklen_t *) &client_len);
+        
+        buffer_push(buffer, conn_fd); // Enviar la conexión al buffer para que los hilos la manejen
     }
+
+    // Unir los hilos (aunque este punto nunca se alcanzará en este diseño)
+    for (int i = 0; i < thread_count; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
     return 0;
 }
